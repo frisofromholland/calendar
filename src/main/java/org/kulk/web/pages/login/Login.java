a47f3160b5a56bccec22253c6f7d82a6bbe9eb4d
frisokulk <frisokulk@lostlemon.nl>
2017-02-21 21:18:58 +0100

    Styling for login page

--- src/main/java/org/kulk/web/pages/login/Login.java
+++ src/main/java/org/kulk/web/pages/login/Login.java
@@ -1,13 +1,17 @@
 package org.kulk.web.pages.login;
 
-import lombok.NoArgsConstructor;
 import org.apache.wicket.markup.html.WebPage;
+import org.kulk.web.panels.Header;
 
 /**
  * User: frisokulk
  * Date: 2/12/17
  * Time: 11:02 PM
  */
-@NoArgsConstructor
 public class Login extends WebPage {
+
+    public Login() {
+	add(new Header("headerPanel"));
+    }
+
 }
